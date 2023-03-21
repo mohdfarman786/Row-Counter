3a98433426ab5a82b78f418cd6044a30f29c23bd
mohdfarman786 <73250066+mohdfarman786@users.noreply.github.com>
2023-03-22 02:14:20 +0530

    abcd

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
-function increment-btn() {
+function increment() {
     count += 1
     countEl.textContent = count
 }
 
-function save-btn() {
+function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
